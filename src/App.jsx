44a6a864736e63a95e186d228ab470fb1fd204f4
bobsabt <bodai.barbara@gmail.com>
2024-05-07 10:30:01 +0200

    add logic and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import { useState } from 'react';
+import numberTostring from './numberTostring' // Assuming numberToString.js is in the same directory
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numberAsString, setNumberAsString] = useState("");
+  const [numberIsValid, setNumberIsValid] = useState(true);
+  const [result, setResult] = useState("");
+
+  // Validation of the user input
+  const onClickConvert = (string) => {
+    const isValid = /^[0-9]+$/.test(string);
+    setNumberIsValid(isValid);
+    if (isValid) {
+      setResult(numberTostring(string));
+    } else {
+      setResult("");
+    }
+  };
+
+  // Handle Enter
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      onClickConvert(numberAsString);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className='container'>
+        <h1>Number Converter</h1>
+        <div className='input-box'>
+          <p>Enter the number</p>
+          <input type="text" placeholder='Write your number here...' value={numberAsString} onChange={e => setNumberAsString(e.target.value)} onKeyUp={handleEnter} />
+        </div>
+        <button onClick={() => onClickConvert(numberAsString)}>Convert</button>
+        <div className="message-box">
+          <p>Result</p>
+          <div>
+            {!numberIsValid ?
+              <p>Please write whole numbers only</p>
+              :
+              <p>{result}</p>
+            }
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
